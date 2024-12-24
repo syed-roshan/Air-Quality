8e20a975a4d8e21e1f062d091a5ed15c6c3d8b66
syed-roshan <syedroshan1554@gmail.com>
2024-12-25 00:05:45 +0530

    Add files via upload

---
+++ src/types/api.ts
@@ -0,0 +1,19 @@
+export interface ThingSpeakResponse {
+  channel: {
+    field1: string;
+    field2: string;
+    field3: string;
+    field4: string;
+    field5: string;
+    field6: string;
+  };
+  feeds: {
+    created_at: string;
+    field1: string;
+    field2: string;
+    field3: string;
+    field4: string;
+    field5: string;
+    field6: string;
+  }[];
+}
